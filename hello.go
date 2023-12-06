bff29cdf0cbeb80a531e864a635df34325085d1e
NoPayNoGame <ok_@live.cn>
2023-12-06 15:53:48 +0800

    新增童æ通过三个不同通过不同裤路径访问不同ç页面

--- hello.go
+++ hello.go
@@ -11,5 +11,12 @@ func main() {
 }
 
 func handlerfunc(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(writer, "<h1>1这是goblog</h1>")
+	if request.URL.Path == "/" {
+		fmt.Fprintf(writer, "<h1>主页</h1>")
+	} else if request.URL.Path == "/about" {
+		fmt.Fprintf(writer, "Orico天下无敌,一支穿云箭,千军万马来相见.")
+		fmt.Fprintf(writer, "\r\n<a href=\"http://www.baidu.com\">官网</a>\r\n<a href=\"\\mailto:zz_@live.cn\\\">联系我们</a>")
+	} else {
+		fmt.Fprintf(writer, "你到底要哦该咯?")
+	}
 }
