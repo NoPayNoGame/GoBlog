a9e1f22bee2892a037dc8d0cdd4f002165d0601a
NoPayNoGame <ok_@live.cn>
2023-12-06 16:12:35 +0800

    设置标头

--- hello.go
+++ hello.go
@@ -11,8 +11,9 @@ func main() {
 }
 
 func handlerfunc(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "text/html; charset=utf-8")
 	if request.URL.Path == "/" {
-		fmt.Fprintf(writer, "<h1>主页</h1>")
+		fmt.Fprintf(writer, "<h1>主页</h1>现在会自动重载了")
 	} else if request.URL.Path == "/about" {
 		fmt.Fprintf(writer, "Orico天下无敌,一支穿云箭,千军万马来相见.")
 		fmt.Fprintf(writer, "\r\n<a href=\"http://www.baidu.com\">官网</a>\r\n<a href=\"\\mailto:zz_@live.cn\\\">联系我们</a>")
