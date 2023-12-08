0004fed1f1d857bca494b3ea58436f7d18d7bc2a
NoPayNoGame <ok_@live.cn>
2023-12-08 15:37:41 +0800

    写完了

--- main.go
+++ main.go
@@ -3,35 +3,48 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 func main() {
-	//http.HandleFunc("/", defaultHandlerfunc)
-	//http.HandleFunc("/about", aboutHandlerfunc)
 	router := http.NewServeMux()
-	router.HandleFunc("/", defaultHandlerfunc)
-	router.HandleFunc("/about", aboutHandlerfunc)
+
+	router.HandleFunc("/", def)
+	router.HandleFunc("/about", about)
+
+	router.HandleFunc("/articles/", func(w http.ResponseWriter, r *http.Request) {
+		id := strings.SplitN(r.URL.Path, "/", 3)[2]
+		fmt.Fprint(w, "文章 ID："+id)
+	})
+
+	router.HandleFunc("/mothod", func(writer http.ResponseWriter, request *http.Request) {
+		switch request.Method {
+		case "POST":
+			fmt.Fprintf(writer, "这是一个POST请求")
+		case "GET":
+			fmt.Fprintf(writer, "这是一个GET请求")
+		default:
+			fmt.Fprintf(writer, "这是一个"+request.Method+"请求")
+		}
+
+	})
+
 	http.ListenAndServe(":3000", router)
 }
 
-func aboutHandlerfunc(writer http.ResponseWriter, request *http.Request) {
+func about(writer http.ResponseWriter, request *http.Request) {
 	writer.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprintf(writer, "\n<a href=\"http://www.baidu.com\">官网</a>\n<a href=\"\\mailto:zz_@live.cn\\\">联系我们</a>")
+	fmt.Fprint(writer, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
+		"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
 }
 
-func defaultHandlerfunc(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(writer, "<h1>主页</h1>")
-}
-
-func handlerfunc(writer http.ResponseWriter, request *http.Request) {
+func def(writer http.ResponseWriter, request *http.Request) {
 	writer.Header().Set("Content-Type", "text/html; charset=utf-8")
 	if request.URL.Path == "/" {
-		fmt.Fprintf(writer, "<h1>主页</h1>现在会自动重载了")
-	} else if request.URL.Path == "/about" {
-		fmt.Fprintf(writer, "Orico天下无敌,一支穿云箭,千军万马来相见.")
-		fmt.Fprintf(writer, "\r\n<a href=\"http://www.baidu.com\">官网</a>\r\n<a href=\"\\mailto:zz_@live.cn\\\">联系我们</a>")
+		fmt.Fprintf(writer, "<h1>这是GoBlog</h1>")
 	} else {
 		writer.WriteHeader(404)
-		fmt.Fprintf(writer, "你到底要哦该咯???")
+		fmt.Fprint(writer, "<h1>请求页面未找到 :(</h1>"+
+			"<p>如有疑惑，请联系我们。</p>")
 	}
 }
